5b0e970441487bd83962c52525c3f5a6da1dea30
jensenhill <91635059+jensenhill@users.noreply.github.com>
2024-10-27 16:36:51 +0000

    Update to Set Exercise 4

--- Set Exercise 4/3. Inserting sample data.sql
+++ Set Exercise 4/3. Inserting sample data.sql
@@ -25,7 +25,13 @@ VALUES
 (1,'Liskeard','Cornwall','England'),
 (2,'Dulverton','Somerset','England'),
 (3,'Porlock','Devon','England'),
-(4,'Devizes','Wiltshire','England');
+(4,'Devizes','Wiltshire','England'),
+(5,'Cheltenham','Gloucestershire','England'),
+(6,'Cricklade','Wiltshire','England'),
+(7, 'Bath','Somerset','England'),
+(8, 'Brighton','East Sussex','England'),
+(9, 'Cambridge','Cambridgeshire','England'),
+(10, 'Cheddar','Somerset','England');
 
 
 -- Create sample data for the Trail table
@@ -36,20 +42,43 @@ VALUES
 (2,'Liskeard Centre','Medium',1,2.9,14.8,38,'Out-and-back',CONVERT(SMALLDATETIME, '2018-12-11 10:00:00')),
 (3,'Exmoor Hike','Hard',2,28.14,105,650,'Loop',CONVERT(SMALLDATETIME, '2019-01-31 08:40:00')),
 (4,'Porlock Marsh Historic Walk','Medium',3,16.05,19.50,120,'Circular',CONVERT(SMALLDATETIME, '2022-04-15 18:10:00')),
-(5,'Historic Buildings of Devizes','Easy',4,2.75,3.10,28,'Out-and-back',CONVERT(SMALLDATETIME, '2021-12-07 21:55:00'));
+(5,'Historic Buildings of Devizes','Easy',4,2.75,3.10,28,'Out-and-back',CONVERT(SMALLDATETIME, '2021-12-07 21:55:00')),
+(6,'Welcome to Bourton-on-the-Water','Easy',5,0.90,0,13,'Circular',CONVERT(SMALLDATETIME, '2024-02-28 15:50:00')),
+(7,'Cricklade to South Cerney','Medium',6,22.5,15,210,'Out-and-back',CONVERT(SMALLDATETIME, '2015-06-06 13:11:00')),
+(8,'Cleeve Hill Circular','Hard',5,7.2,42,90,'Circular',CONVERT(SMALLDATETIME, '2011-05-30 09:25:00')),
+(9,'Bath Skyline Walk','Medium', 7, 6.0, 300, 120,'Loop',CONVERT(SMALLDATETIME, '2023-05-15 10:00:00')),
+(10,'Brighton Seafront Stroll','Easy', 8, 4.3, 0, 68,'Circular',CONVERT(SMALLDATETIME, '2009-06-20 23:05:00')),
+(11,'Cambridge Historical Route','Medium', 9, 5.1, 20, 80,'Out-and-back',CONVERT(SMALLDATETIME, '2024-07-10 05:00:00')),
+(12,'Cheddar Gorge Caving','Hard', 10, 7, 270, 148,'Loop',CONVERT(SMALLDATETIME, '2019-08-18 06:43:00'));
+
 
 -- Create sample data for the Trail_Feature table
 INSERT INTO CW1.Trail_Feature(trail_id,feature_id)
 VALUES
-(0,0), --Trail 0
 (0,1), --Trail 0
-(1,2), --Trail 1
-(1,3), --Trail 1
-(2,4), --Trail 2
-(2,5), --Trail 2
-(3,6), --Trail 3
+(0,2), --Trail 0
+(1,6), --Trail 1
+(1,0), --Trail 1
+(2,6), --Trail 2
+(2,2), --Trail 2
 (3,7), --Trail 3
-(4,8), --Trail 4
-(4,9), --Trail 4
-(5,1), --Trail 5
-(5,2); --Trail 5
+(3,8), --Trail 3
+(4,4), --Trail 4
+(4,5), --Trail 4
+(5,0), --Trail 5
+(5,2), --Trail 5
+(6,7), --Trail 6
+(6,1), --Trail 6
+(6,2), --Trail 6
+(7,3), --Trail 7
+(7,2), --Trail 7
+(8,9), --Trail 8
+(8,6), --Trail 8
+(8,5), --Trail 8
+(9,6), --Trail 9
+(9,8), --Trail 9
+(10,1), --Trail 10
+(10,2), --Trail 10
+(11, 4),--Trail 11
+(11,5), --Trail 11
+(12,9); --Trail 12
